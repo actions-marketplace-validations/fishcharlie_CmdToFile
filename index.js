1ee70699f523b13b83e8cbd299415fb067c90737
Charlie Fish <fishcharlie@me.com>
2022-07-17 18:00:08 -0600

    Adding support for data input

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const core = require("@actions/core");
-// const exec = require("@actions/exec");
 const fs = require("fs").promises;
 const execCmd = require("child_process").exec;
 
-const settings = ["command", "output"].reduce((obj, key) => {
+const settings = ["command", "data", "output"].reduce((obj, key) => {
 	obj[key] = core.getInput(key);
 	return obj;
 }, {});
 
 (async () => {
-	const result = await exec(settings.command);
+	if (!command && !data) {
+		throw new Error("Either command or data is required as an input.");
+	}
+
+	const result = data || await exec(settings.command);
 	await fs.writeFile(settings.output, result);
-
-	// let myOutput = "";
-
-	// const options = {};
-	// options.listeners = {
-	// 	"stdout": (data) => {
-	// 		myOutput += data.toString();
-	// 	}
-	// };
-	// await exec.exec(settings.command, [], options);
-
-	// await fs.writeFile(settings.output, myOutput);
 })();
 
 function exec(cmd) {
